6a5cb0f92dcec2ea267340f755e663f15ee9341b
luqman1434 <145521860+luqman1434@users.noreply.github.com>
2023-10-05 10:53:42 +0800

    Update map_area.py

--- map_area.py
+++ map_area.py
@@ -24,16 +24,14 @@ if __name__ == '__main__':
     st.title('Available ITP companies in Malaysia')
 
     file_input = 'MMU ITP List 13_9_9_11.xlsx'
+    geojson_file = "msia_district.geojson"
 
     text_load_state = st.text('Reading files ...')
+    with open(geojson_file, encoding='utf-8', errors='ignore') as gj_f:
+        geojson_data = gpd.read_file(gj_f)
+
     itp_list_state = read_file(file_input, 0)
     text_load_state.text('Reading files ... Done!') 
-
-    # Get unique states from the Excel file
-    unique_states = itp_list_state['STATE'].unique()
-
-    # Add a checkbox to allow user to select states
-    selected_states = st.multiselect('Select States', unique_states)
 
     map_size = Figure(width=800, height=600)
     map_my = folium.Map(location=[4.2105, 108.9758], zoom_start=6)
@@ -45,13 +43,15 @@ if __name__ == '__main__':
     itp_list_state['geometry'] = itp_list_state.apply(lambda x: Point(x['map_longitude'], x['map_latitude']), axis=1)
     itp_list_state = gpd.GeoDataFrame(itp_list_state, geometry='geometry')
 
-    # Filter ITP data by selected states
-    itp_list_state_filtered = itp_list_state[itp_list_state['STATE'].isin(selected_states)]
+    joined_data = gpd.sjoin(geojson_data, itp_list_state, op="contains").groupby(["NAME_1", "NAME_2"]).size().reset_index(name="count")
+
+    merged_gdf = geojson_data.merge(joined_data, on=["NAME_1", "NAME_2"], how="left")
+    merged_gdf['count'].fillna(0, inplace=True)
 
     threshold_scale = [0, 1, 2, 4, 8, 16, 32, 64, 128, 200, 300, 400] 
 
     text_load_state.text('Plotting ...')
-    for itp_data in itp_list_state_filtered.to_dict(orient='records'):
+    for itp_data in itp_list_state.to_dict(orient='records'):
         latitude = itp_data['map_latitude']
         longitude = itp_data['map_longitude']
         company_name = itp_data['Company name']
